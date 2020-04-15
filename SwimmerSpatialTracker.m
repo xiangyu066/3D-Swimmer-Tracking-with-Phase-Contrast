92269327a6ff35702807b3c75bc30d0acd1e39e4
莊翔淯 (Xiang-Yu, Zhuang) <55974558+xiangyu066@users.noreply.github.com>
2020-04-15 12:15:09 +0800

    Add files via upload

--- SwimmerSpatialTracker.m
+++ SwimmerSpatialTracker.m
@@ -3,12 +3,13 @@ echo on
 % - Title: SwimmerSpatialTracker.m
 % - Author: XYZ
 % - Created date: April 9, 2020
-% - Modified date: April 13, 202
+% - Modified date: April 15, 202
 % - Notes:
-%       1.) 
+%       1.) This version have to manually choose target, and the target
+%       have to roughly put in the middle.
 % - Next modified:
 %       1.) Automatically chosen target
-% - Version: 1.0.0
+% - Version: 1.1.0
 % - Environments: Win10 (64-bit) / MATLAB 2019a (64-bit)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 echo off
@@ -19,37 +20,36 @@ disp('Running...')
 global um px sec msec
 um = 1;
 px = 1;
-sec = 1;
-msec = 1E-3 *(sec);
+msec = 1;
+sec = 1E+3 *(msec);
 
 %% Define parameters of imaging system
 % define imaging system parameters 
 dz = 0.25 *(um);
 pixelsize = 6.5 *(um);
-Mag = 40; % magnification of objective
+Obj_Mag = 40; % magnification of objective
 dt = 30 *(msec);
+nFrames = 100;
 
 %
 speed_upper = 100 *(um/sec);
-inputfile = 'D:\20200409\Motile test\RP437_ROI038_BF.tif';
-outputdir = 'D:\20200409\Analysis\';
-outputname = 'RP437_ROI038_BF-3';
-
-start_frame = 1;
-end_frame = 100;
+inputfile = 'C:\Users\motorsgroup\Downloads\20200409\RP437\Motile test\RP437_ROI052_BF.tif';
+outputdir = 'C:\Users\motorsgroup\Downloads\20200409\RP437';
+outputname = 'RP437_ROI052_BF-2';
 
 %% Preallocating 1vavriables and functions
 % Load depth library
 load('Library.mat')
 load('Library_Pos.mat')
-
 Lib_Square = size(Library,1);
 nLayers = size(Library,3);
+
+% normalized library
 Library = Library - mean(mean(Library,2),1); % remove mean value to obtain variation
 Library = Library./std(Library,0,[1, 2]); % normalized
 
 % Preallocating vavriables
-eff_pixelsize = pixelsize/Mag;
+eff_pixelsize = pixelsize/Obj_Mag;
 search_Box_xy = Lib_Square + 2*round(speed_upper*dt/eff_pixelsize);
 search_Box_z = 2*round(speed_upper*dt/dz);
 
@@ -58,24 +58,25 @@ Lib_padding = zeros([search_Box_xy,search_Box_xy,nLayers]);
 Lib_padding(1:Lib_Square,1:Lib_Square,:) = Library;
 
 %
-idx = [];
+maxCrr_nLayer = 0;
 
 % Make database folder
-% [~,msg] = mkdir('Analysis');
+[~,msg] = mkdir('Analysis');
 
 %%
-vSession = VideoWriter([outputdir,outputname,'.avi']);
+vSession = VideoWriter([outputdir,'\Analysis\',outputname,'.avi']);
 vSession.FrameRate = 33;
 open(vSession)
 
 figure(1), set(gcf,'WindowStyle','docked')
-for nFrame = start_frame:end_frame
+for nFrame = 1:nFrames
     
     % read image
     origina = double(imread(inputfile,nFrame));
     
-    % human-made define target
-    if nFrame ==start_frame
+    % label tracking seeds
+    if (nFrame ==1)
+        % human-made define target
         figure(2), imshow(origina,[])
         h = imrect(gca,[1, 1, search_Box_xy, search_Box_xy]);
         position = wait(h);
@@ -86,7 +87,7 @@ for nFrame = start_frame:end_frame
         sLayer = 1;
         eLayer = nLayers;
     else
-        if ~isempty(idx)
+        if ~isempty(maxCrr_nLayer)
             sx = sx+shiftX+Lib_Square/2-search_Box_xy/2;
             sy = sy+shiftY+Lib_Square/2-search_Box_xy/2;
             ex = sx+search_Box_xy-1;
@@ -96,11 +97,18 @@ for nFrame = start_frame:end_frame
         eLayer = old_nLayer+search_Box_z/2;
     end
     
+    % determinate whether out of boundary
+    checksum = (sx>=1)+(sy>=1)+(ex<=size(origina,2))+(ey<=size(origina,1));
+    if checksum<4
+        break;
+    end
+    
+    % normalized
     neworigina = origina(sy:ey,sx:ex);
     neworigina = neworigina-mean2(neworigina);
     neworigina = neworigina./std2(neworigina);
     
-    % using FFT to reduce calculating correlation time
+    % using FFT to boost calculating correlation efficiency
     tic
     Crrs = zeros(nLayers,4);
     for nLayer = sLayer:eLayer
@@ -110,7 +118,7 @@ for nFrame = start_frame:end_frame
     end
     toc
     
-    % filt out of boundary 
+    % filt out of boundary
     if nFrame>1
         Crrs((Crrs(:,3)==0),:) = '';
         Crrs((Crrs(:,1)>size(neworigina,2)-Lib_Square+1),:) = '';
@@ -118,23 +126,23 @@ for nFrame = start_frame:end_frame
     end
     
     % find maximal correlation coefficient
-    idx = find(Crrs(:,4)==max(Crrs(:,4)));
-    if isempty(idx)
+    maxCrr_nLayer = find(Crrs(:,4)==max(Crrs(:,4)));
+    if isempty(maxCrr_nLayer)
         shiftX = 0;
         shiftY = 0;
     else
-        shiftX = Crrs(idx,1);
-        shiftY = Crrs(idx,2);
+        shiftX = Crrs(maxCrr_nLayer,1);
+        shiftY = Crrs(maxCrr_nLayer,2);
         old_shiftX = shiftX;
         old_shiftY = shiftY;
-        old_nLayer = Crrs(idx,3);
+        old_nLayer = Crrs(maxCrr_nLayer,3);
     end
 
     % write databse
-    if isempty(idx)
-        data(nFrame,:) = [[sx+shiftX,sy+shiftY]*eff_pixelsize,data(nFrame-1,3)];
+    if isempty(maxCrr_nLayer)
+        Pos(nFrame,:) = [[sx+shiftX,sy+shiftY]*eff_pixelsize,Pos(nFrame-1,3)];
     else
-        data(nFrame,:) = [[sx+shiftX,sy+shiftY]*eff_pixelsize,Library_Pos(Crrs(idx,3))];
+        Pos(nFrame,:) = [[sx+shiftX,sy+shiftY]*eff_pixelsize,Library_Pos(Crrs(maxCrr_nLayer,3))];
     end
     
     % real-time monitoring
@@ -143,10 +151,10 @@ for nFrame = start_frame:end_frame
     subplot 221, imshow(neworigina,[]), title(['nFrame = ', num2str(nFrame)])
     hold on, rectangle('position',[old_shiftX,old_shiftY,Lib_Square,Lib_Square],'edgecolor','r')
     
-    subplot 222, plot3(data(1,1),data(1,2),data(1,3), 's','Color','r','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
-    hold on, plot3(data(:,1),data(:,2),data(:,3),'k-')
-    hold on, plot3(data(end,1),data(end,2),data(end,3), '>','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
-    xlabel('X [\mum]'), ylabel('Y [\mum]'), zlabel('Z [\mum]')
+    subplot 222, plot3(Pos(1,1),Pos(1,2),Pos(1,3), '>','Color','r','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+    hold on, plot3(Pos(:,1),Pos(:,2),Pos(:,3),'k-')
+    hold on, plot3(Pos(end,1),Pos(end,2),Pos(end,3), 's','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+    xlabel('X [\mum]','fontweight','bold'), ylabel('Y [\mum]','fontweight','bold'), zlabel('Z [\mum]','fontweight','bold')
     grid on
     
     subplot 223, imshow(neworigina(old_shiftY:old_shiftY+Lib_Square-1,old_shiftX:old_shiftX+Lib_Square-1), []), title('target roi')
@@ -164,26 +172,44 @@ for nFrame = start_frame:end_frame
     writeVideo(vSession, frame);
 end
 close(vSession)
-save([outputdir,outputname,'.mat'],'data')
 
-%%
-%
-figure, set(gcf,'WindowStyle','docked')
-subplot 221, plot3(data(2:end,1),data(2:end,2),data(2:end,3))
-xlabel('X [\mum]'), ylabel('Y [\mum]'), zlabel('Z [\mum]')
+%% plot swimmer's trajectory
+figure(3), set(gcf,'WindowStyle','docked')
+subplot 221, plot3(Pos(1,1),Pos(1,2),Pos(1,3), '>','Color','r','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+hold on, plot3(Pos(:,1),Pos(:,2),Pos(:,3),'k')
+hold on, plot3(Pos(end,1),Pos(end,2),Pos(end,3), 's','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+xlabel('X [\mum]','fontweight','bold'), ylabel('Y [\mum]','fontweight','bold'), zlabel('Z [\mum]','fontweight','bold')
 grid on, axis equal
-subplot 222, plot(data(2:end,1),data(2:end,2))
-xlabel('X [\mum]'), ylabel('Y [\mum]')
+
+subplot 222, plot(Pos(1,1),Pos(1,2), '>','Color','r','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+hold on, plot(Pos(end,1),Pos(end,2), 's','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+hold on, plot(Pos(:,1),Pos(:,2),'k')
+xlabel('X [\mum]','fontweight','bold'), ylabel('Y [\mum]','fontweight','bold')
+legend({'Start','End'})
 grid on, axis equal
-subplot 223, plot(data(2:end,1),data(2:end,3))
-xlabel('X [\mum]'), ylabel('Z [\mum]')
+
+subplot 223, plot(Pos(1,1),Pos(1,3), '>','Color','r','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+hold on, plot(Pos(end,1),Pos(end,3), 's','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+hold on, plot(Pos(:,1),Pos(:,3),'k')
+xlabel('X [\mum]','fontweight','bold'), ylabel('Z [\mum]','fontweight','bold')
+legend({'Start','End'})
 grid on, axis equal
-subplot 224, plot(data(2:end,2),data(2:end,3))
-xlabel('Y [\mum]'), ylabel('Z [\mum]')
+
+subplot 224, plot(Pos(1,2),Pos(1,3), '>','Color','r','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+hold on, plot(Pos(end,2),Pos(end,3), 's','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF')
+hold on, plot(Pos(:,2),Pos(:,3),'k')
+xlabel('Y [\mum]','fontweight','bold'), ylabel('Z [\mum]','fontweight','bold')
+legend({'Start','End'})
 grid on, axis equal
 
 frame = getframe(gcf);
-imwrite(frame.cdata, [outputdir,outputname,'.png'])
+imwrite(frame.cdata, [outputdir,'\Analysis\',outputname,'.png'])
+
+% save tracking data
+data.unit = {'um', 'msec'};
+data.dt = dt;
+data.Pos = Pos;
+save([outputdir,'\Analysis\',outputname,'.mat'], 'data')
 
 %%
 disp('Done.')
